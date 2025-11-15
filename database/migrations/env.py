0fbf7205dd07652f5d29d20e6546ddfdc5e537f9
Rashid Mustafaev <rash20071601@gmail.com>
2025-11-15 11:39:54 +0300

    Update database\migrations\env.py

--- database/migrations/env.py
+++ database/migrations/env.py
@@ -4,28 +4,31 @@ import asyncio
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy import pool
 from alembic import context
+
 from database.models import Base
 from database.connection import get_db_url
 from config.config import load_config
 
-# Alembic Config object
 config = context.config
 
-# Логирование
+# Логи
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+# Модели
 target_metadata = Base.metadata
 
-# Загружаем конфиг
+# Конфиги
 cfg = load_config()
 
+
 def get_url() -> str:
-    # Для asyncpg оставляем +asyncpg
+    """Получаем async URL"""
     return get_db_url(cfg)
 
-def run_migrations_offline() -> None:
-    """Генерация SQL без подключения к БД (offline)"""
+
+def run_migrations_offline():
+    """Offline режим (генерация SQL без подключения к БД)"""
     url = get_url()
     context.configure(
         url=url,
@@ -37,27 +40,32 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+
 async def run_migrations_online_async():
-    """Асинхронные миграции с реальным подключением"""
+    """Online миграции с async connect"""
     connectable = create_async_engine(
         get_url(),
         poolclass=pool.NullPool,
     )
 
     async with connectable.connect() as connection:
-        await connection.run_sync(lambda conn: context.configure(
-            connection=conn,
-            target_metadata=target_metadata
-        ))
+        await connection.run_sync(lambda sync_conn:
+            context.configure(
+                connection=sync_conn,
+                target_metadata=target_metadata
+            )
+        )
 
         async with connection.begin():
-            await connection.run_sync(lambda conn: context.run_migrations())
+            await connection.run_sync(lambda sync_conn: context.run_migrations())
 
     await connectable.dispose()
 
-def run_migrations_online() -> None:
-    """Запуск асинхронных миграций через asyncio"""
+
+def run_migrations_online():
+    """Запуск миграций через asyncio"""
     asyncio.run(run_migrations_online_async())
+
 
 if context.is_offline_mode():
     run_migrations_offline()
